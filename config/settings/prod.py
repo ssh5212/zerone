e6e6da884be6118cae91131e37ca043b275111b8
AngelPlayer <ssh5212@naver.com>
2021-09-05 09:54:09 +0900

    ALLOWED_HOSTS 변경

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -1,6 +1,6 @@
 from .base import *
 
-ALLOWED_HOSTS = ['3.37.176.114']
+ALLOWED_HOSTS = ['3.37.176.114', 'younghana.co.kr']
 STATIC_ROOT = BASE_DIR / 'static/'
 STATICFILES_DIRS = []
 DEBUG = False
